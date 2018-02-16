b58cbd5484612cbd3ee7f0fbc7414d40d82f3aeb
Sebastian Tengborg <sebastian.tengborg@king.com>
2018-02-16 23:42:29 +0100

    la till fler bolag att sortera ut
    Joinade in dessa bolag i huvud data frame

--- aktie_scrap.R
+++ aktie_scrap.R
@@ -1,14 +1,30 @@
 # To do:
 # 1. Ta bort alla banker, investmentbolag och fastighetsbolag som inte är
-# top 2 i resp kategori efter första sorteringen
+# top 2 i resp kategori efter första sorteringen.
+# 2. Behåll bara B aktien om det finns flera.
 
 # Loading the rvest package
 library(rvest)
 library(dplyr)
 
-banker <- read.csv(file = "banker.txt", sep = "\n")
-fastighet <- read.csv(file = "fastighet.txt", sep = "\n")
-investment <- read.csv(file = "investment.txt", sep = "\n")
+# Läser in aktier som vi vill kunna filtrera ut
+banker <- read.csv(file = "banker.txt", sep = "\n", stringsAsFactors = FALSE)
+fastigheter <- read.csv(file = "fastigheter.txt", sep = "\n", stringsAsFactors = FALSE)
+investmentbolag <- read.csv(file = "investmentbolag.txt", sep = "\n", stringsAsFactors = FALSE)
+abaktier <- read.csv(file = "abaktier.txt", sep = "\n", stringsAsFactors = FALSE)
+
+banker$bank <- 1
+banker$banker <- tolower(banker$banker) 
+
+fastigheter$fastigheter <- 1
+fastigheter$fastighet <- tolower(fastigheter$fastighet)
+
+investmentbolag$invest <- 1
+investmentbolag$investment <- tolower(investmentbolag$investment)
+
+abaktier$abaktier <- tolower(abaktier$abaktier)
+abaktier$abaktie <- 1
+
 # Specifirerar vilken hemsida vi vill läsa data från
 url <- 'https://www.nordnet.se/mux/web/marknaden/kurslista/aktier.html?marknad=Sverige&lista=1_1&large=on&sektor=0&subtyp=key_ratios&sortera=&sorteringsordning='
 
@@ -40,20 +56,33 @@ df_alles$PE<- gsub(',', '.', df_alles$PE)
 df_alles$EK<- gsub(',', '.', df_alles$EK)
 df_alles$DA<- gsub(',', '.', df_alles$DA)
 
-clean_df <- df_alles %>% mutate(PE = as.numeric(as.character(PE)),
+clean_df <- df_alles %>% mutate(Aktie = tolower(Aktie),
+                                PE = as.numeric(as.character(PE)),
                                  EK = as.numeric(as.character(EK)),
                                  DA = as.numeric(as.character(DA)))
 
 
 # Filtrera ut aktier som inte möter villkoren för köp
 tmp_kop <- clean_df %>%
-    mutate(Aktie = tolower(Aktie), 
-           EP = (1/PE)*100) %>% 
+    mutate(EP = (1/PE)*100) %>% 
   filter(PE > 0, DA > 0) %>% 
-    arrange(desc(EP)) %>% mutate(a = row_number()) %>% 
-    arrange(EK) %>% mutate(b = row_number()) %>% 
-    mutate(c = a + b) %>% arrange(c, DA)
-    
+  left_join(abaktier, by = c("Aktie" = "abaktier")) %>% 
+  left_join(banker, by = c("Aktie" = "banker")) %>% 
+  left_join(fastigheter, by = c("Aktie" = "fastighet")) %>% 
+  left_join(investmentbolag, by = c("Aktie" = "investment"))
+
+# kop <- tmp_kop %>% 
+#   
+#   
+#   arrange(desc(EP)) %>% mutate(a = row_number())
+# 
+# %>% 
+#   left_join(abaktier, by = c("abaktier", "Aktie"))
+# 
+# %>% 
+#     arrange(EK) %>% mutate(b = row_number()) %>% 
+#     mutate(c = a + b) %>% arrange(c, DA)
+#     
 
 # Analysen:
 # Steg 1: EP-tal, högst EP tal högst upp
